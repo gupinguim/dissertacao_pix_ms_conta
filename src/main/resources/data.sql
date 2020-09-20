cc984e011c852202cf4a5a1574fa4a36866dc590
Augusto Almeda Correia <augusto.a.correia@gmail.com>
2020-09-19 21:37:28 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 insert into dict(id,telefone,email,cpf_cnpj,uuid) values (1,'5511993822541','joao@teste.com','12345678909','97aabdbe-e9f2-11ea-adc1-0242ac120002')
 insert into conta_pi(id,saldo_total,saldo_bloqueado,situacao_conta,id_cliente,data_criacao,fk_dict) values (1,5000.00,0.00,'Ativa',1,'2020-08-29',1)
+insert into dict(id,telefone,email,cpf_cnpj,uuid) values (2,'5511969092327','maria@teste.com','12345789000101','4eaf6006-f7d0-11ea-adc1-0242ac120002')
+insert into conta_pi(id,saldo_total,saldo_bloqueado,situacao_conta,id_cliente,data_criacao,fk_dict) values (2,100000.00,0.00,'Ativa',2,'2020-08-29',2)
